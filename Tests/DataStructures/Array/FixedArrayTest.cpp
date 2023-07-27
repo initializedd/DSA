bafb12412bffcdfdc2d4bcc7513d5faae160c236
initializedd <bertzhallows@gmail.com>
2023-07-27 01:17:40 +0100

    Organised tests

--- Tests/DataStructures/Array/FixedArrayTest.cpp
+++ Tests/DataStructures/Array/FixedArrayTest.cpp
@@ -1,69 +1,70 @@
 #include <catch2/catch_test_macros.hpp>
 #include "../../../DataStructures/Array/FixedArray.hpp"
 
-TEST_CASE(".insert()")
+TEST_CASE("FixedArray {}")
 {
-	FixedArray<int, 5> arr;
+	FixedArray<int, 3> arr { -1, 893, 12345 };
+
+	REQUIRE(arr[0] == -1);
+	REQUIRE(arr[1] == 893);
+	REQUIRE(arr[2] == 12345);
+}
+
+TEST_CASE("FixedArray .insert()")
+{
+	FixedArray<int, 3> arr;
 	arr.insert(22, 0);
 	arr.insert(326, 1);
 	arr.insert(-85325, 2);
-	arr.insert(11916062, 3);
-	arr.insert(-7685151, 4);
 
 	REQUIRE(arr[0] == 22);
 	REQUIRE(arr[1] == 326);
 	REQUIRE(arr[2] == -85325);
-	REQUIRE(arr[3] == 11916062);
-	REQUIRE(arr[4] == -7685151);
 }
 
-TEST_CASE("[] insertion")
+TEST_CASE("FixedArray .front()")
 {
-	FixedArray<int, 5> arr;
-	arr[0] = -4;
-	arr[1] = 23;
-	arr[2] = 5151;
-	arr[3] = -77353; 
-	arr[4] = 0;
+	FixedArray<int, 3> arr{ -50, 20, 1415 };
 
-	REQUIRE(arr[0] == -4);
-	REQUIRE(arr[1] == 23);
-	REQUIRE(arr[2] == 5151);
-	REQUIRE(arr[3] == -77353);
-	REQUIRE(arr[4] == 0);
-}
-
-TEST_CASE(".front()")
-{
-	FixedArray<int, 5> arr{-50, 20, 1415, -3151, 2};
-	
 	REQUIRE(arr.front() == -50);
 }
 
-TEST_CASE(".back()")
+TEST_CASE("FixedArray .back()")
 {
-	FixedArray<int, 5> arr{404, 4141, -737, 6452, 0};
+	FixedArray<int, 3> arr{ 404, 4141, -737 };
 
-	REQUIRE(arr.back() == 0);
+	REQUIRE(arr.back() == -737);
 }
 
-TEST_CASE(".size()")
+TEST_CASE("FixedArray .size()")
 {
-	FixedArray<int, 5> arr;
+	FixedArray<int, 3> arr;
 
-	REQUIRE(arr.size() == 5);
+	REQUIRE(arr.size() == 3);
 
 	SECTION("Size does not change when using .insert()")
 	{
 		arr.insert(32, 0);
 
-		REQUIRE(arr.size() == 5);
+		REQUIRE(arr.size() == 3);
 	}
 
 	SECTION("Size does not change when using [] overload")
 	{
-		arr[3] = 20;
+		arr[2] = 20;
 
-		REQUIRE(arr.size() == 5);
+		REQUIRE(arr.size() == 3);
 	}
+}
+
+TEST_CASE("FixedArray [] Insertion")
+{
+	FixedArray<int, 3> arr;
+	arr[0] = -4;
+	arr[1] = 23;
+	arr[2] = 5151;
+
+	REQUIRE(arr[0] == -4);
+	REQUIRE(arr[1] == 23);
+	REQUIRE(arr[2] == 5151);
 }
